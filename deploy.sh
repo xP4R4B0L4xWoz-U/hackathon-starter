3cec620425b0ef86d0ccc79342ac4c067bf63142
xP4R4B0L4xWoz-U <xp4r4b0l4x@hotmail.com>
2021-11-11 15:06:43 -0600

    test commit

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-echo "Installing 'node-cmd'"
+echo "Installing 'node-cmd'..."
 npm install node-cmd
-echo "Installing 'node-ssh'"
+echo "Installing 'node-ssh'..."
 npm install node-ssh
-echo "Running 'deploy.js'"
+echo "Running 'deploy.js'..."
 node deploy.js
